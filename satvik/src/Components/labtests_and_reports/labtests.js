bfae75877d7cfece19947f593f09cf4252edd6c4
Prakash9019 <plsprakash2003@gmail.com>
2023-04-23 22:58:53 +0530

    f

--- satvik/src/Components/labtests_and_reports/labtests.js
+++ satvik/src/Components/labtests_and_reports/labtests.js
@@ -2,18 +2,27 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 
 const labtests = () => {
+  const [currentForm, setCurrentForm] = useState(null);
+
+  const handleClick = (form) => {
+    setCurrentForm(form);
+  };
   return (
     <nav>
    
     <div className="labtests">
         <div className="row">
-        <Link className="labtest-list" to='/labtestcategories#bt1'>Blood Tests</Link>
-        <Link className="labtest-list" to='/labtestcategories#bt2'>Kidney tests</Link>
+        <Link className="labtest-list" to='/labtestcategories/bt1' onClick={() => handleClick('formOne')} >Blood Tests</Link>
+        <Link className="labtest-list" onClick={() => handleClick('formOne')}  to='/labtestcategories/bt2'>Kidney tests</Link>
         </div>
         <div className="row">
-        <Link className="labtest-list" to='/labtestcategories#bt3'>Thyroid function test</Link>
-        <Link className="labtest-list" to='/labtestcategories#bt4'>Urinanalysis</Link>
+        <Link className="labtest-list" onClick={() => handleClick('formOne')} to='/labtestcategories/bt3'>Thyroid function test</Link>
+        <Link className="labtest-list" onClick={() => handleClick('formOne')} to='/labtestcategories/bt4'>Urinanalysis</Link>
         </div>
+        {currentForm === 'formOne' && <BodyTemperature />}
+         {currentForm === 'formTwo' && <PulseRate />}
+         {currentForm === 'formThree' && <BreathingRate />}
+         {currentForm === 'formFour' && <BloodPressure />}
     </div>
     
 </nav>
