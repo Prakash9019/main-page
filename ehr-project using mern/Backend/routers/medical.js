4c240c752f42091a68a3fb970b9e68cf4df153bc
Prakash9019 <plsprakash2003@gmail.com>
2023-05-19 12:52:12 +0530

    added files

--- ehr-project using mern/Backend/routers/medical.js
+++ ehr-project using mern/Backend/routers/medical.js
@@ -8,22 +8,10 @@ var path = require('path');
 
 var app = express();
 
-  app.set('views', path.join(__dirname, 'views')); 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: false})); 
-app.use(express.json());
+ var bodyParser = require('body-parser')
 
-var bodyParser = require('body-parser')
-
-
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true,parameterLimit:100000,limit:"50mb" }))
 app.use(bodyParser.json())
-// parse application/json
-
-
-// POST /login gets urlencoded bodies
 
 var multer = require('multer');
  
@@ -32,7 +20,7 @@ var storage = multer.diskStorage({
         cb(null, 'uploads')
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, file.fieldname)
     }
 });
 //, limits:{fileSize: maxSize} 
@@ -42,69 +30,20 @@ var upload = multer({ storage: storage});
 
  
 router.post('/upload-image', upload.single('myfile'), async (req, res) => {
-    console.log("oye....");
     let image = (req.file) ? req.file.filename :null;
-    console.log("it is image"+image);
-    console.log("hello");
-    var obj = {
-        name: req.body.name,
-        img: {
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + myfile.filename)),
-            contentType: 'image/png'
-        }
-    }
-    Model.create(obj).then ((err, item) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // item.save();
-            console.log("all done");
-            res.redirect('/');
-        }
+  
+   let data=new Model();
+   data.name=req.file.originalname;
+  // console.log(path.join(__dirname + '/uploads/'+req.file.originalname));
+   fs.writeFileSync(req.file.originalname,req.file.filename);
+   data.img.data=fs.readFileSync(path.join("D:/mini-pro/main-page/ehr-project using mern/" + req.file.originalname));
+   data.img.contentType='image/png';
+   const savedNote=await data.save();
+   console.log(savedNote);
+   res.json(savedNote);
+   
     });
-//     const {originalname,mimetype,path} =req.file;
 
-//  const image=new Model({
-//     name:originalname,
-//     contentType:mimetype,
-//     data: {
-//            type:"Buffer",
-//             data: fs.readFileSync(path),
-//         },
-//         user:req.user.id
-//  })
-//  console.log("image is saved");
-//  const savedNote=await image.save();
-//  res.json(savedNote);
-//  console.log("image is saved");
-});
-
-// router.post('/upload-image',urlencodedParser,async (req,res)=>{
-//     try{
-//      const {base64} =req.body;
-//      console.log("ok"+ base64)
-//        // If there are errors, return Bad request and the errors
-//        const errors = validationResult(req);
-//        if (!errors.isEmpty()) {
-//            return res.status(400).json({ errors: errors.array() });
-//        }
-      
-//        const note=new File({
-//            base64,user:req.user.id
-//        })
-//        console.log("hi" + note);
-//        const savedNote=await note.save();
-//        res.json(savedNote);
-//     //    Image.create({myfile:base64});
-//     //    res.send({status:"ok"});
-      
-//     }
-//     catch(error){
-//          return res.status(400).json({error:error.message});
-//     }
-//     }
-// );
 
 module.exports=router;
 
